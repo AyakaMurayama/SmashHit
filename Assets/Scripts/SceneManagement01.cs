78b8e961de2f888c8d522e1dac39899be093cbd0
みょんた <murayamaayaka@MMMacBook-Pro.local>
2017-10-03 03:17:41 +0900

    時間制限とシーン移動

--- Assets/Scripts/SceneManagement01.cs
+++ Assets/Scripts/SceneManagement01.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneManagement01 : MonoBehaviour {
+public class SceneManagement01 : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+
+    public void Buttonpush()
+    {
+        SceneManager.LoadScene("main");
+    }
 }
---
+++ Assets/Scripts/SceneManagement02.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManagement02 : MonoBehaviour
+{
+    Text timetext;
+    float time = 30.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        timetext = GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        timetext.text = time.ToString("f2");
+
+        if (time < 0.03f)
+        {
+            SceneManager.LoadScene("End");
+        }
+
+    }
+}
