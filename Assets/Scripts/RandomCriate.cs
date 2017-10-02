cbeba1cbdfff1733275eee86974c1eca51bef1c6
みょんた <murayamaayaka@MMMacBook-Pro.local>
2017-10-03 03:44:38 +0900

    生成したオブジェクトを子供要素にした

--- Assets/Scripts/RandomCriate.cs
+++ Assets/Scripts/RandomCriate.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RandomCriate : MonoBehaviour
 {
     int[] select = new int[2];
-    float[] objy = { 8f, 12f, 16f, 20f, 24f, 28f, 32f };
+    float[] objy = { 8f, 12f, 16f, 20f, 24f, 28f, 32f, 36f };
     int randomy;
     public GameObject[] objselect;
     int number;
@@ -15,15 +15,27 @@ public class RandomCriate : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        for (int i = 1; i <= 20; i++)
+        for (int i = 1; i <= 15; i++)
         {
+            var parent = GameObject.Find("1").transform;
             number = Random.Range(0, objselect.Length);
             //number = Enumerable.Range(0, objselect.Length).OrderBy(n => Guid.NewGuid()).Take(5);
             objx = Random.Range(-2.83f, 2.83f);
-            randomy = Random.Range(0, 6);
+            randomy = Random.Range(0, 4);
             objpos = new Vector2(objx, objy[randomy]);
 
-            Instantiate(objselect[number], objpos, transform.rotation);
+            Instantiate(objselect[number], objpos, transform.rotation, parent);
+        }
+        for (int i = 1; i <= 15; i++)
+        {
+            var parent = GameObject.Find("2").transform;
+            number = Random.Range(0, objselect.Length);
+            //number = Enumerable.Range(0, objselect.Length).OrderBy(n => Guid.NewGuid()).Take(5);
+            objx = Random.Range(-2.83f, 2.83f);
+            randomy = Random.Range(4, 7);
+            objpos = new Vector2(objx, objy[randomy]);
+
+            Instantiate(objselect[number], objpos, transform.rotation, parent);
         }
 
     }
