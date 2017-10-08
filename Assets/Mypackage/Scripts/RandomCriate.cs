1e6ef9347c9db9e5a9f2b35844a03cccd533e137
みょんた <>
2017-10-09 03:21:14 +0900

    ステージ増設

--- Assets/Mypackage/Scripts/RandomCriate.cs
+++ Assets/Mypackage/Scripts/RandomCriate.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class RandomCriate : MonoBehaviour
 {
-    float[] objy = { 8f, 12f, 16f, 20f, 24f, 28f, 32f, 36f };
-    float[] levely = { 9f, 17f, 25f, 33f, };
+    float[] objy = { 8f, 12f, 16f, 20f, 24f, 28f, 32f, 36f, 40f, 44f, 48f, 52f, 56f, 60f, 64f, 68f };
+    float[] levely = { 9f, 17f, 25f, 33f, 41f, 49f, 57f, 65f };
+    //float[] inky = { 10f, 14f, 28f, 22f, 26f, 30f, 34f, 38f, 42f, 46f, 50f, 54f, 58f, 62f };
 
     //float[] objx = { -2.83f, -1.0f, 1.0f, 2.83f };
     int randomy, randomx1;
@@ -78,31 +79,66 @@ public class RandomCriate : MonoBehaviour
                     Instantiate(objselect[number], objpos, transform.rotation, parent);
                 }
             }
+            else if (i >= 9 && i <= 12)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    var parent = GameObject.Find("3").transform;
+                    number = Random.Range(0, objselect.Length);
+                    randomy = Random.Range(0, 4);
+                    int randomIndex = Random.Range(0, tempNumsx.Count);
+                    Debug.Log(tempNumsx[randomIndex]);
+
+
+                    objpos = new Vector2(tempNumsx[randomIndex], objy[i]);
+                    tempNumsx.Remove(randomIndex);
+                    Instantiate(objselect[number], objpos, transform.rotation, parent);
+                }
+            }
+            else
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    var parent = GameObject.Find("4").transform;
+                    number = Random.Range(0, objselect.Length);
+                    randomy = Random.Range(0, 4);
+                    int randomIndex = Random.Range(0, tempNumsx.Count);
+                    Debug.Log(tempNumsx[randomIndex]);
+
+
+                    objpos = new Vector2(tempNumsx[randomIndex], objy[i]);
+                    tempNumsx.Remove(randomIndex);
+                    Instantiate(objselect[number], objpos, transform.rotation, parent);
+                }
+            }
         }
         //numink = Random.Range(1, 8); //回す数
-        List<float> inkMunsy = new List<float>() { 10f, 14f, 28f, 22f, 26f, 30f, 34f, 38f };
+        List<float> inkMunsy = new List<float>() { 10f, 14f, 28f, 22f, 26f, 30f, 34f, 38f, 42f, 46f, 50f, 54f, 58f, 62f };
         List<float> inkMunsx = new List<float>() { -2.5f, -2f, -1.5f, -1.0f, 2.5f, 2f, 1.5f, 1.0f };
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < 12; i++)
         {
-            int randomIndex = Random.Range(0, inkMunsy.Count);
+            int randomIndex = Random.Range(0, 8);
+            int randomIndexy = Random.Range(0, inkMunsy.Count);
             numberink = Random.Range(0, ink.Length);
-            Debug.Log(inkMunsy[randomIndex]);
-            inkMunsy.Remove(randomIndex);
-            inkMunsy.Remove(randomIndex);
-            inkpos = new Vector2(inkMunsx[randomIndex], inkMunsy[randomIndex]);
+            //Debug.Log(inkMunsy[randomIndex]);
+            //inkMunsy.Remove(randomIndex);
+            //inkMunsy.Remove(randomIndex);
+            inkpos = new Vector2(inkMunsx[randomIndex], inkMunsy[randomIndexy]);
             Instantiate(ink[numberink], inkpos, transform.rotation);
+            inkMunsy.Remove(randomIndexy);
 
         }
-        numn = Random.Range(1, 3); //回す数
-        List<float> enNums = new List<float>() { 10f, 18f, 26f }; //ガラガラの中身
+
+        List<float> enNums = new List<float>() { 10f, 18f, 26f, 34f, 42f, 50f, 58f, 66f }; //ガラガラの中身
+        numn = Random.Range(4, enNums.Count); //回す数
 
         for (int i = 0; i < numn; i++)
         {
             int randomIndex = Random.Range(0, enNums.Count);
             Debug.Log(enNums[randomIndex]);
-            enNums.Remove(randomIndex);
             enpos = new Vector2(Random.Range(-2.5f, 2.0f), enNums[randomIndex]);
             Instantiate(en, enpos, transform.rotation);
+            enNums.Remove(randomIndex);
         }
 
         for (int k = 0; k < levely.Length; k++)
